71b96ad7ed563fc7f7259a7b8dc8d3b68d2a381b
anoushk1234 <anoushka1828@gmail.com>
2022-01-06 17:10:36 +0530

    finished :)

--- programs/gmchat/src/lib.rs
+++ programs/gmchat/src/lib.rs
@@ -1,14 +1,42 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7DWkoJ87sdv4cMTYbWvB5uxLn6nEZwKAbSqkxCzm9oqr");
 
 #[program]
-pub mod gmchat {
+pub mod gm_chat {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
+        Ok(())
+    }
+    pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+#[account]
+pub struct BaseAccount {
+    pub data: String,
+    pub data_list: Vec<String>,
+}
--- tests/gmchat.js
+++ tests/gmchat.js
@@ -1,14 +1,58 @@
 const anchor = require('@project-serum/anchor');
-
-describe('gmchat', () => {
-
+const {SystemProgram} = anchor.web3;
+const {Program} = require('@project-serum/anchor');
+const {PublicKey} = require('@solana/web3.js');
+const idl=require("../target/idl/gm_chat.json");
+// describe('gmchat', () => {
+const main = async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+ // const network = clusterApiUrl('devnet');
 
-  it('Is initialized!', async () => {
+  // it('Is initialized!', async () => {
     // Add your test here.
-    const program = anchor.workspace.Gmchat;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+    console.log(anchor.workspace);
+    const programID = new PublicKey(idl.metadata.address);
+
+    const program = new Program(idl, programID, provider);
+    const baseAccount = anchor.web3.Keypair.generate();
+    console.log("ping")
+    console.log(program,programID,provider,baseAccount);
+    const tx = await program.rpc.initialize("My first message", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount]
+    });
+    console.log("📝 Your transaction signature", tx);
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data: ', account.data);
+
+    const tx2 = await program.rpc.update("gm2", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Updated data: ", account.data);
+    console.log("All account data: ", account);
+    console.log("All data: ", account.dataList);
+
+//   });
+// });
+  }
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
